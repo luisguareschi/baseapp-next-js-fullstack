bbcc3bf7a0ee161e23c968ab493e4b887f2e3913
luisguareschi <luisguareschi@hotmail.com>
2025-10-22 12:19:42 +0200

    Add protected session handling in auth module

---
+++ app/api/me/route.ts
@@ -0,0 +1,25 @@
+import { protectedSession } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const { session, unauthorizedResponse } = await protectedSession(req);
+
+  if (!session) {
+    return unauthorizedResponse;
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: session.user.id,
+    },
+  });
+
+  if (!user) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+
+  return NextResponse.json({
+    user,
+  });
+}
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -10,3 +11,22 @@ export const auth = betterAuth({
     enabled: true,
   },
 });
+
+export const protectedSession = async (req: NextRequest) => {
+  const session = await auth.api.getSession({
+    headers: req.headers,
+  });
+  let unauthorizedResponse: NextResponse | null = null;
+
+  if (!session) {
+    unauthorizedResponse = NextResponse.json(
+      { error: "Unauthorized" },
+      { status: 401 },
+    );
+  }
+
+  return {
+    session,
+    unauthorizedResponse,
+  };
+};
